7d4fa5f6ee61bcbb4ba339992a8e2b6282d3db68
Csaba Fazekas <csabafazekasster@gmail.com>
2014-08-13 13:38:07 +0200

    js animation2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     $(".about-content").hide();
     $(".internet-content > p").hide();
+    
+    $('.hero-typo > span').hide();
 
     $(document).scroll(function() {
         var docScroll = $(document).scrollTop();
-        var boxCntOfset = $(".about").offset().top - 100;
+        var boxCntOfset = $(".quick-navigation").offset().top - 100;
         var boxCntOfsetInternet = $(".internet1").offset().top - 100;
+        var boxCntOfsetContact = $(".reference-block3").offset().top - 100;
 
         if (docScroll >= boxCntOfset) {
             $(".about-content").fadeIn(600);
@@ -13,9 +16,13 @@ $(document).ready(function() {
         if (docScroll >= boxCntOfsetInternet) {
          $(".internet-photo-left").animate({left: '0px'},200);
          $(".internet-photo-right").delay(100).animate({right: '0px'},200);
-            $("h2").delay(200).animate({top: '1050px'},200);
+            $("h2").delay(200).animate({top: '80px'},200);
             // console.log("h2 fired");
             $(".internet-content > p").delay(250).fadeIn(600);
+        }
+        if (docScroll >= boxCntOfsetContact) {
+            $(".personal-details").animate({left: '0px'},200);
+            $(".datas").delay(100).delay(100).animate({right: '0px'},200);
         }
 
     });
